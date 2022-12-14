6604843adc8e19c420ea669687addcff0df404b6
Kanaparthi Phani <38982795+kanaparthiphani1@users.noreply.github.com>
2022-12-14 13:03:50 +0530

    feat: Added logger middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
-const iceCreamActions =
-  require("./features/iceCream/iceCreamSlice").iceCreamActions;
+const iceCreamActions = require("./features/iceCream/iceCreamSlice")
+  .iceCreamActions;
 
-console.log("Initial state :  " + JSON.stringify(store.getState()));
-
-store.subscribe(() => {
-  console.log("Updated state  : " + JSON.stringify(store.getState()));
-});
+store.subscribe(() => {});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
